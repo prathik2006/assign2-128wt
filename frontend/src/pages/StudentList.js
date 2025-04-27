1232f9b03d0137d979ce1469043b7a81a05842d5
Prathik <prathik06202006@gmail.com>
2025-04-27 12:03:05 +0530

    commited

--- frontend/src/pages/StudentList.js
+++ frontend/src/pages/StudentList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { fetchStudents, deleteStudent } from './api';  // Import API functions
+import { fetchStudents, deleteStudent } from '../api'; 
 
 const StudentList = () => {
   const [students, setStudents] = useState([]);
